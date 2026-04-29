7f0e4ad16d0fc11b9a5aa6f97122b13ec1eb6352
aluno <e023aluno@outlook.com>
2026-04-28 21:44:59 -0300

    Corrigido entrada com acc do google

--- app/src/app/login/login.page.ts
+++ app/src/app/login/login.page.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { 
-  IonContent, IonHeader, IonTitle, IonToolbar, IonCard, 
-  IonCardHeader, IonCardTitle, IonCardContent, IonItem, 
-  IonLabel, IonInput, IonButton 
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar, IonCard,
+  IonCardHeader, IonCardTitle, IonCardContent, IonItem,
+  IonLabel, IonInput, IonButton, IonIcon
 } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup
+} from '@angular/fire/auth';
 
-// --- IMPORTS DO FIREBASE ---
-import { Auth, signInWithEmailAndPassword, onAuthStateChanged } from '@angular/fire/auth';
+import { addIcons } from 'ionicons';
+import { logoGoogle } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-login',
@@ -18,24 +26,22 @@ import { Auth, signInWithEmailAndPassword, onAuthStateChanged } from '@angular/f
   styleUrls: ['./login.page.scss'],
   standalone: true,
   imports: [
-    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, 
-    FormsModule, IonCard, IonCardHeader, IonCardTitle, 
-    IonCardContent, IonItem, IonLabel, IonInput, IonButton
+    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule,
+    FormsModule, IonCard, IonCardHeader, IonCardTitle,
+    IonCardContent, IonItem, IonLabel, IonInput, IonButton,IonIcon
   ]
 })
 export class LoginPage implements OnInit {
-  // O Firebase usa E-mail, então certifique-se que o campo no HTML envie um e-mail válido
-  username: string = ''; 
+  username: string = '';
   password: string = '';
 
   constructor(
     private router: Router,
     private toastController: ToastController,
-    private auth: Auth // Injeção do Firebase Auth
-  ) { }
+    private auth: Auth
+  ) { addIcons({ logoGoogle }); }
 
   ngOnInit() {
-
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
         this.router.navigate(['/tabs/tab1']);
@@ -50,23 +56,27 @@ export class LoginPage implements OnInit {
     }
 
     try {
-  
       await signInWithEmailAndPassword(this.auth, this.username, this.password);
-      
       this.showToast('Bem-vindo ao sistema Luy-83!');
       this.router.navigate(['/tabs/tab1']);
     } catch (error: any) {
       let mensagem = 'Erro ao entrar';
-
-    
       if (error.code === 'auth/invalid-credential') {
         mensagem = 'E-mail ou senha incorretos.';
       } else if (error.code === 'auth/invalid-email') {
         mensagem = 'Formato de e-mail inválido.';
       }
-
       this.showToast(mensagem);
-      console.error('Erro no login:', error);
+    }
+  }
+
+  async loginComGoogle() {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(this.auth, provider);
+      this.showToast('Login com Google realizado!');
+    } catch (error: any) {
+      this.showToast('Erro ao autenticar com o Google.');
     }
   }
 
